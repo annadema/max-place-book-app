f7ef89ceb6a2bb4f9ed82514a22fed104954380c
aladar <annamaria.demartino@gmail.com>
2023-06-05 18:45:11 +0200

    Ho apportato le modifiche citate nella lezione 67 ma ho un errore su ionicons che non è riconosciuto Ho installato il package ma continua a non funzionare la line di codice con l'import nel main
    Gli altri file moficati sono
    app-routing.module.ts
    app.module.ts
    
    In app.component.tsnon era presente la liena di codice da rimuovere
    quindi non è stato modificato
    
    Invece ho docuto commentare l'import di ionicons
    e quindi alla fine la riga di codice
    
    defineCustomElements(window);

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import {
+  RouteReuseStrategy,
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -9,7 +14,15 @@ import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    IonicModule,
+    RouterLink,
+    RouterLinkActive,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
